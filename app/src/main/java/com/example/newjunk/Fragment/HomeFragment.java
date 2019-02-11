c150ed115e094cce8983989223331521925a263d
Dibakar Jayjot <dibakarjayjot@gmail.com>
2019-02-11 22:35:08 +0530

    All Code

---
+++ app/src/main/java/com/example/newjunk/Fragment/HomeFragment.java
@@ -0,0 +1,102 @@
+package com.example.newjunk.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.example.newjunk.Adapter.PostAdapter;
+import com.example.newjunk.Model.Post;
+import com.example.newjunk.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postLists;
+    private List<String> followingList;
+
+    ProgressBar progressBar;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        recyclerView = view.findViewById(R.id.recycle_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        postLists = new ArrayList<>();
+        postAdapter= new PostAdapter(getContext(), postLists);
+        recyclerView.setAdapter(postAdapter);
+        progressBar = view.findViewById(R.id.progress_circular);
+        checkFollowing();
+        return view;
+    }
+    private void checkFollowing(){
+        followingList = new ArrayList<>();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Follow")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                followingList.clear();
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    followingList.add(snapshot.getKey());
+
+                }
+                readPosts();
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+    private void readPosts(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                postLists.clear();
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+                    for(String id:followingList){
+                        if(post.getPublisher().equals(id)){
+                            postLists.add(post);
+                        }
+                    }
+                }
+                postAdapter.notifyDataSetChanged();
+
+                progressBar.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+}
